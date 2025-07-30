421f383a559a292c5ad371a132f09ce633564164
Olga Cuevas i Melis <56602289+guitacm@users.noreply.github.com>
2025-07-30 19:28:34 +0200

    Update CartaForm.jsx

--- src/components/CartaForm.jsx
+++ src/components/CartaForm.jsx
@@ -26,7 +26,7 @@ const formatCategoria = (cat) => {
 
 const CartaForm = ({ index, plat, actualitzarPlat, eliminarPlat }) => {
   return (
-    <div className="carta-form-linea">
+    <div className="carta-form-linea no-print">
       <label className="visible-checkbox">
         <input
           type="checkbox"
